e6c65cddb6f2263c481fe11630d5968702800308
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-18 10:47:33 -0800

    Added inline styling

--- app/assets/javascripts/components/hello_world.js.jsx
+++ app/assets/javascripts/components/hello_world.js.jsx
@@ -2,10 +2,14 @@ var HelloWorld = React.createClass({
 
   render: function() {
     var someVar = "This is text from a variable.";
+    var pStyle = {
+      fontSize: '24px',
+      color: 'red'
+    };
     return (
         <div class="hello-world-component">
           <h1>Hello World!</h1>
-          <p>{someVar}</p>
+          <p style={pStyle}>{someVar}</p>
           <HelloWorldMessage text="This is a test message" />
           <HelloWorldMessage text="So is this." />
           <HelloWorldMessage text="And another one" />
