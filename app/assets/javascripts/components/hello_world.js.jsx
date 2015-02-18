623dddcade6a32bb19584380e61d3241095007e1
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-18 11:50:05 -0800

    Made countdown dynamic with a form

--- app/assets/javascripts/components/hello_world.js.jsx
+++ app/assets/javascripts/components/hello_world.js.jsx
@@ -4,15 +4,40 @@ var HelloWorld = React.createClass({
     alert('Countdown completed!');
   },
 
+  getInitialState: function() {
+    return {
+      secondsStartFrom: 0
+    }
+  },
+
+  updateSecondsStartFrom: function() {
+    var input = this.refs.secondsInputEl.getDOMNode();
+    this.setState({secondsStartFrom: parseInt(input.value)});
+  },
+
+  startCountdown: function(e) {
+    e.preventDefault();
+    this.setState({countdownVisible: true});
+  },
+
   render: function() {
     var someVar = "This is text from a variable.";
     var pStyle = {
       fontSize: '24px',
       color: 'red'
     };
+
+    var countdownEl;
+    if (this.state.countdownVisible) {
+      countdownEl = <Countdown start={this.state.secondsStartFrom} onComplete={this.doneCountdown} />
+    }
+
     return (
         <div class="hello-world-component">
-          <Countdown start={5} onComplete={this.doneCountdown} />
+          {countdownEl}
+          <form onSubmit={this.startCountdown}>
+            <input type="text" defaultValue={this.state.secondsStartFrom} ref="secondsInputEl" onChange={this.updateSecondsStartFrom} />
+          </form>
           <h1>Hello World!</h1>
           <p style={pStyle}>{someVar}</p>
           <HelloWorldMessage type="important" text="This is a test message" />
